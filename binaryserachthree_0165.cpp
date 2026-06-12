6aaada8fd750d61a0144abd956183f2f343d9e8e
Semutmanis <aldidermawan1@gmail.com>
2026-06-12 15:05:41 +0700

    membuat class dan memberi akses modiefier juga mendeklarasi variabel dan object node

--- binaryserachthree_0165.cpp
+++ binaryserachthree_0165.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+//membuaut class node
+class Node{
+  
+  //memberi akses modifier serta mendeklarasi variabel dan object node
+public:
+  string info;
+  Node* leftChild;
+  Node* rightChild;
+
+}
