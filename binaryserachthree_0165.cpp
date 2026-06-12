432ff028695c140125a8411ed53210161c817a71
Semutmanis <aldidermawan1@gmail.com>
2026-06-12 15:17:19 +0700

    melanjutkan

--- binaryserachthree_0165.cpp
+++ binaryserachthree_0165.cpp
@@ -37,5 +37,9 @@ public:
     newNode->info = element;
     newNode->leftChild = nullptr;
     newNode->rightChild = nullptr;
+
+    Node* parent = nullptr;
+    Node* currentNode = nullptr;
+    search(element, parent, currentNode);
   }
 }
